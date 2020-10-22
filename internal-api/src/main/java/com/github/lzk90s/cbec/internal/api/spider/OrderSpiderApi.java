fed1147915c8f0e0f41391b305247a76964b23f4
liuzhikun <zk.liu@hzgosun.com>
2020-10-22 17:40:36 +0800

    update

--- internal-api/src/main/java/com/github/lzk90s/cbec/internal/api/spider/OrderSpiderApi.java
+++ internal-api/src/main/java/com/github/lzk90s/cbec/internal/api/spider/OrderSpiderApi.java
@@ -2,13 +2,14 @@ package com.github.lzk90s.cbec.internal.api.spider;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 public interface OrderSpiderApi {
-    @GetMapping("/list_unhandled_order")
-    List<OrderDTO> listUnhandledOrder(@RequestParam("platform") String platform,
+    @GetMapping("/list_unhandled_order/{platform}")
+    List<OrderDTO> listUnhandledOrder(@PathVariable("platform") String platform,
                                       @RequestParam("user") String user,
                                       @RequestParam("password") String password);
 }
